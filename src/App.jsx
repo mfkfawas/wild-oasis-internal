5b52cdd069e005ccb8498218e6a6d78913a3d056
mfkawas <fawas.zivratech.net>
2024-07-19 23:34:19 +0300

    334. Building the App Layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,38 @@
-import styled from 'styled-components';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import GlobalStyles from './styles/GlobalStyles';
-import Button from './ui/Button';
-import Input from './ui/Input';
-import Heading from './ui/Heading';
-import Row from './ui/Row';
-
-const StyledApp = styled.div`
-  padding: 20px;
-`;
+import DashBoard from './pages/Dashboard';
+import Bookings from './pages/Bookings';
+import Cabins from './pages/Cabins';
+import Users from './pages/Users';
+import Settings from './pages/Settings';
+import Account from './pages/Account';
+import Login from './pages/Login';
+import PageNotFound from './pages/PageNotFound';
+import AppLayout from './ui/AppLayout';
 
 function App() {
   return (
-    <>
-      <GlobalStyles />
-      <StyledApp>
-        <Row>
-          <Row type="horizontal">
-            <Heading as="h1">Hello World</Heading>
-            <div>
-              <Heading as="h2">Check In and Out</Heading>
-              <Button onClick={() => alert('Check In')}>Check In</Button>
-              <Button
-                variation="secondary"
-                size="small"
-                onClick={() => alert('Check Out')}
-              >
-                Check Out
-              </Button>
-            </div>
-          </Row>
-
-          <Row>
-            <Heading as="h3">Form</Heading>
-            <form>
-              <Input type="number" placeholder="Number of guests" />
-              <Input type="number" placeholder="Number of guests" />
-            </form>
-          </Row>
-        </Row>
-      </StyledApp>
-    </>
+    <div>
+      <>
+        <GlobalStyles />
+        <BrowserRouter>
+          <Routes>
+            {/* This is called a layout route bcz it doesnt have a path prop */}
+            <Route element={<AppLayout />}>
+              <Route index element={<Navigate replace to="dashboard" />} />
+              <Route path="/dashboard" element={<DashBoard />} />
+              <Route path="/bookings" element={<Bookings />} />
+              <Route path="/cabins" element={<Cabins />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="/settings" element={<Settings />} />
+              <Route path="/account" element={<Account />} />
+            </Route>
+            <Route path="/login" element={<Login />} />
+            <Route path="/*" element={<PageNotFound />} />
+          </Routes>
+        </BrowserRouter>
+      </>
+    </div>
   );
 }
 
---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,33 @@
+import { Outlet } from 'react-router-dom';
+import Header from './Header';
+import Sidebar from './Sidebar';
+import styled from 'styled-components';
+
+const StyledAppLayout = styled.div`
+  display: grid;
+  grid-template-columns: 26rem 1fr;
+  grid-template-rows: auto 1fr;
+  height: 100dvh;
+`;
+
+const Main = styled.main`
+  background-color: var(--color-grey-50);
+  padding: 4rem 4.8rem 6.4rem;
+`;
+
+function AppLayout() {
+  return (
+    <StyledAppLayout>
+      <Header />
+      <Sidebar />
+      {/* we r keeping main as container here to have all the pages to have same style 
+      bcz main will be the parent element for all pages and inside pages component will
+      be wrapped by fragment */}
+      <Main>
+        <Outlet />
+      </Main>
+    </StyledAppLayout>
+  );
+}
+
+export default AppLayout;

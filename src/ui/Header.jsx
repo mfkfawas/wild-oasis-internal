4eaa2a5ead201164ca202661e8968836669947f7
mfkawas <fawas.zivratech.net>
2024-07-21 13:12:14 +0300

    335. Building the Sidebar and Main Navigation

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,6 +4,7 @@ const StyledHeader = styled.header`
   background-color: var(--color-grey-0);
   padding: 1.2rem 4.8rem;
   border-bottom: 1px solid var(--color-grey-100);
+  /* grid-column: 1/-1; */
 `;
 
 function Header() {

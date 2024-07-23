d4bbce82edd73f07d161451b2c2cb9dd0a1842fc
mfkawas <fawas.zivratech.net>
2024-07-23 11:13:52 +0300

    348. added react-query

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import GlobalStyles from './styles/GlobalStyles';
 import DashBoard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
@@ -11,28 +14,34 @@ import PageNotFound from './pages/PageNotFound';
 import AppLayout from './ui/AppLayout';
 
 function App() {
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      staleTime: 60 * 1000,
+    },
+  });
+
   return (
-    <div>
-      <>
-        <GlobalStyles />
-        <BrowserRouter>
-          <Routes>
-            {/* This is called a layout route bcz it doesnt have a path prop */}
-            <Route element={<AppLayout />}>
-              <Route index element={<Navigate replace to="dashboard" />} />
-              <Route path="/dashboard" element={<DashBoard />} />
-              <Route path="/bookings" element={<Bookings />} />
-              <Route path="/cabins" element={<Cabins />} />
-              <Route path="/users" element={<Users />} />
-              <Route path="/settings" element={<Settings />} />
-              <Route path="/account" element={<Account />} />
-            </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="/*" element={<PageNotFound />} />
-          </Routes>
-        </BrowserRouter>
-      </>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+
+      <GlobalStyles />
+      <BrowserRouter>
+        <Routes>
+          {/* This is called a layout route bcz it doesnt have a path prop */}
+          <Route element={<AppLayout />}>
+            <Route index element={<Navigate replace to="dashboard" />} />
+            <Route path="/dashboard" element={<DashBoard />} />
+            <Route path="/bookings" element={<Bookings />} />
+            <Route path="/cabins" element={<Cabins />} />
+            <Route path="/users" element={<Users />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/account" element={<Account />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+          <Route path="/*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 

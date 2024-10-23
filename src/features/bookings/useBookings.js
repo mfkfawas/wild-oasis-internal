d5bcebaca0f53d3df7b06d20e0dba6a7c59eaefa
mfkfawas <mfkfawas@gmail.com>
2024-10-23 13:38:44 +0300

    feat(ui): invalidate-query-notes

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -10,7 +10,13 @@ export function useBookings() {
   const filter =
     !filterValue || filterValue === 'all'
       ? null
-      : { field: 'totalPrice', value: 5000, method: 'gte' };
+      : // : { field: 'totalPrice', value: 5000, method: 'gte' };
+        { field: 'status', value: filterValue };
+
+  // INVALIDATE-QUERY-NOTES
+  // 1) By default, queryClient.invalidateQueries({ queryKey: ['cabins'] }) will invalidate ALL cabins with or without parameters, so in general you don't need to worry about additional parameters.
+  // 2) With queryClient.invalidateQueries({ queryKey: ['cabins'], {...some params here...} }) you will invalidate ONLY this specific query, but not main cabins
+  // 3) With queryClient.invalidateQueries({ queryKey: ['cabins'], exact: true }) you will invalidate ONLY cabins
 
   const {
     isLoading,

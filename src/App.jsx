0fc8bf93fed9a90cebae9519e67f00b3818cb16a
mfkfawas <mfkfawas@gmail.com>
2024-10-26 22:17:49 +0300

    feat(ui): single-booking-page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
+import { Toaster } from 'react-hot-toast';
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 import GlobalStyles from './styles/GlobalStyles';
+import AppLayout from './ui/AppLayout';
+
 import DashBoard from './pages/Dashboard';
 import Bookings from './pages/Bookings';
 import Cabins from './pages/Cabins';
@@ -11,8 +14,7 @@ import Settings from './pages/Settings';
 import Account from './pages/Account';
 import Login from './pages/Login';
 import PageNotFound from './pages/PageNotFound';
-import AppLayout from './ui/AppLayout';
-import { Toaster } from 'react-hot-toast';
+import Booking from './pages/Booking';
 
 function App() {
   const queryClient = new QueryClient({
@@ -36,6 +38,7 @@ function App() {
             <Route index element={<Navigate replace to="dashboard" />} />
             <Route path="/dashboard" element={<DashBoard />} />
             <Route path="/bookings" element={<Bookings />} />
+            <Route path="/bookings/:bookingId" element={<Booking />} />
             <Route path="/cabins" element={<Cabins />} />
             <Route path="/users" element={<Users />} />
             <Route path="/settings" element={<Settings />} />

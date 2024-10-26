e2067f32d8c079640ef03bf7f763f18c54e6f8be
mfkfawas <mfkfawas@gmail.com>
2024-10-26 21:46:18 +0300

    feat(ui): prefetch-bookings

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,8 +1,11 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
 import useURL from '../../hooks/useURL';
+import { PAGE_SIZE } from '../../utils/constants';
 
 export function useBookings() {
+  const queryClient = useQueryClient();
+
   const { searchParam: sortByValue } = useURL('sortBy', 'startDate-desc');
   const { searchParam: filterValue } = useURL('filter');
   const { searchParam: page } = useURL('page');
@@ -23,6 +26,7 @@ export function useBookings() {
   // 2) With queryClient.invalidateQueries({ queryKey: ['cabins'], {...some params here...} }) you will invalidate ONLY this specific query, but not main cabins
   // 3) With queryClient.invalidateQueries({ queryKey: ['cabins'], exact: true }) you will invalidate ONLY cabins
 
+  // QUERY
   const {
     isLoading,
     data: { data: bookings, count } = {},
@@ -31,6 +35,21 @@ export function useBookings() {
     queryKey: ['bookings', filter, sortBy, page],
     queryFn: () => getBookings({ filter, sortBy, page }),
   });
+
+  // PRE-FETCHING
+  const pageCount = Math.ceil(count / PAGE_SIZE);
+
+  if (page < pageCount)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page + 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page + 1 }),
+    });
+
+  if (page > 1)
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sortBy, page - 1],
+      queryFn: () => getBookings({ filter, sortBy, page: page - 1 }),
+    });
 
   return { isLoading, error, bookings, count };
 }

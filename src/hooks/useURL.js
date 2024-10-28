cc624df4d0a18e04843138cd82e3a869f9a5b16c
mfkfawas <mfkfawas@gmail.com>
2024-10-28 22:24:53 +0300

    feat(ui): useURL-supports-default-page-initialization

--- src/hooks/useURL.js
+++ src/hooks/useURL.js
@@ -1,18 +1,28 @@
-import { useSearchParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { useLocation, useSearchParams } from 'react-router-dom';
 
 // if we are fetching the value of the below arra of fields, we need to convert it to number
 const NUMERIC_FIELDS = ['page'];
+const PAGE_ROUTES = ['/bookings'];
 
 // defaultValue is '' bcz in select if we pass '', it will automatcially select the first option. But in others(Filter) we need to explicitly pass the default value as filter is not build using select
 function useURL(field, defaultValue = '') {
+  const location = useLocation();
   const [searchParams, setSearchParams] = useSearchParams();
   const searchParam = searchParams.get(field) || defaultValue;
 
   function setSearchParam(value) {
     searchParams.set(field, value);
-    // if (searchParams.get('page')) searchParams.set('page', 1);
     setSearchParams(searchParams);
   }
+
+  // Initially set page to 1 if we are on a page that has pagination
+  useEffect(() => {
+    if (PAGE_ROUTES.includes(location.pathname)) {
+      if (!searchParams.get('page')) searchParams.set('page', 1);
+      setSearchParams(searchParams);
+    }
+  }, []);
 
   return {
     searchParam: NUMERIC_FIELDS.includes(field)

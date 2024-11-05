3d42fd1ebdaad1e30bbf52bd693447b998d37d2f
mfkfawas <mfkfawas@gmail.com>
2024-11-05 21:52:02 +0300

    chore(ui): cleanup

--- src/features/authentication/useLogin.jsx
+++ src/features/authentication/useLogin.jsx
@@ -9,8 +9,7 @@ export function useLogin() {
 
   const { mutate: login, isLoading } = useMutation({
     mutationFn: ({ email, password }) => loginApi({ email, password }),
-    onSuccess: user => {
-      console.log(user);
+    onSuccess: () => {
       navigate('/dashboard');
     },
     onError: err => {

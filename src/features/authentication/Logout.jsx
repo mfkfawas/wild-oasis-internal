2f5ccbf66b92c5b927330f61f74b5aa07bdd5ac9
mfkfawas <mfkfawas@gmail.com>
2024-11-09 00:07:09 +0300

    feat(ui): logout-success

--- src/features/authentication/Logout.jsx
+++ src/features/authentication/Logout.jsx
@@ -1,10 +1,14 @@
 import { HiArrowRightOnRectangle } from 'react-icons/hi2';
+import { useLogout } from './useLogout';
+import SpinnerMini from '../../ui/SpinnerMini';
 import ButtonIcon from '../../ui/ButtonIcon';
 
 function Logout() {
+  const { logout, isLoading } = useLogout();
+
   return (
-    <ButtonIcon>
-      <HiArrowRightOnRectangle />
+    <ButtonIcon disabled={isLoading} onClick={logout}>
+      {isLoading ? <SpinnerMini /> : <HiArrowRightOnRectangle />}
     </ButtonIcon>
   );
 }

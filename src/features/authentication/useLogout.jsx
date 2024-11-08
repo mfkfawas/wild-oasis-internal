a87aa9ba70156d7902a688d954b370a865b679ee
mfkfawas <mfkfawas@gmail.com>
2024-11-08 23:36:45 +0300

    feat(ui): remove-queries

--- src/features/authentication/useLogout.jsx
+++ src/features/authentication/useLogout.jsx
@@ -1,13 +1,16 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { useNavigate } from 'react-router-dom';
 import { logout as logoutApi } from '../../services/apiAuth';
 
 export function useLogout() {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
 
   const { mutate: logout, isLoading } = useMutation({
     mutationFn: logoutApi,
     onSuccess: () => {
+      // remove all the queries from the query cache
+      queryClient.removeQueries();
       navigate('/login', { replace: true });
     },
   });

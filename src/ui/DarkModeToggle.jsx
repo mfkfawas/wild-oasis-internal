0ca47d633e6cd03ed2c801f61490187830ee1d2f
mfkfawas <mfkfawas@gmail.com>
2024-12-29 05:40:15 +0300

    feat(ui): added DarkModeToggle component skeleton

---
+++ src/ui/DarkModeToggle.jsx
@@ -0,0 +1,10 @@
+import { HiOutlineMoon } from 'react-icons/hi2';
+import ButtonIcon from './ButtonIcon';
+
+export default function DarkModeToggle() {
+  return (
+    <ButtonIcon>
+      <HiOutlineMoon />
+    </ButtonIcon>
+  );
+}
